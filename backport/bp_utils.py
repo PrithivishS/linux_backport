08f720929a887d3a73823d242113c17d91dd8fab
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-02-04 17:03:17 +0000

    add status message to menu if tree unclean

--- backport/bp_utils.py
+++ backport/bp_utils.py
@@ -59,9 +59,12 @@ def print_patch_list(msg, patch_list, fobj):
         i += 1
     print_log("<<<<<: %s" % (msg), fobj)
 
-def show_menu(menu):
+def show_menu(menu, state):
     for (ix, d) in zip(range(len(menu) + 1), menu):
         print(str(ix) + ": " + menu[ix]['prompt'])
+    (ret, tmp) = general_shell_cmd("git status --porcelain")
+    if tmp:
+        print_log("\n** working tree UNCLEAN **", state['log_fobj'])
 
 def do_menu_choice(menu, state):
     while True:
