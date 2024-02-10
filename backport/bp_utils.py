3a07eac14837ba7bd4f47bc78e6e3254875be870
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-02-10 18:25:30 +0000

    add poll_shell_cmd()
    add poll_shell_cmd()

--- backport/bp_utils.py
+++ backport/bp_utils.py
@@ -6,6 +6,9 @@ import glob
 from git_utils import *
 import git_utils
 from datetime import datetime
+import select
+import shutil
+import shlex
 
 def confirm(prompt, legal_response_list):
     tmp = input(prompt)
@@ -190,6 +193,31 @@ def general_shell_cmd(cmd):
     result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
     return (result.returncode, result.stdout.decode('latin-1'))
 
+def poll_shell_cmd(cmd, shell_args, line_fn, state):
+    cmd = shutil.which(cmd)
+    shell_args = shlex.split(shell_args)
+    cmd = [ cmd] + shell_args
+    shell_args = ' '.join(shell_args)
+    
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    # Create empty buffers and a polling object
+    poll = select.poll()
+    poll.register(process.stdout, select.POLLIN)
+    poll.register(process.stderr, select.POLLIN)
+
+    # Continuously poll the stdout and stderr streams to capture output as it
+    # appears
+    spew = []
+    while process.poll() is None:
+        rlist, _, _ = select.select([process.stdout, process.stderr], [], [])
+        for stream in rlist:
+            line = stream.readline().decode().rstrip("\n")
+            spew.append(line)
+            line_fn(line, state)
+          
+    return spew
+              
 def state_init(args): # anoter obvious objuect
     state = dict()
     state['cp_num'] = next_cp_num(args)
