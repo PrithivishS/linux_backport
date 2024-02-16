d18d95381336f39cc4fbffdc4aab824ad46bc800
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-02-16 20:40:07 +0000

    show line number in git_short_log()

--- backport/git_utils.py
+++ backport/git_utils.py
@@ -27,11 +27,15 @@ def git_status(state):
     print_log(s, state)
     
 def git_short_log(fmt, state):
-    range = "%s^..HEAD" % (state['first_commit'])
-    cmd = "git log " + range + " --pretty=tformat:'%<(10) %h  %<(12) %an : %s'"
+    commit_range = "%s^..HEAD" % (state['first_commit'])
+    cmd = "git log " + commit_range + " --pretty=tformat:'%<(10) %h  %<(12) %an : %s'"
     result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
     s = result.stdout.decode().strip()
-    print_log(s.rstrip("\n"), state)
+    lines = s.split("\n")
+    n = len(lines)
+
+    for (ix, line)  in zip(range(1, len(lines), 1), lines):
+        print_log(str(ix) + ": " + line, state)
     
 def git_get_commit_date(commitish):
     cmd = "git log -1 --pretty=format:'%ad' --date=format:'%m/%d/%y' " + commitish
