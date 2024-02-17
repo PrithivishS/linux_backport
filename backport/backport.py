28c040d98e3f589f8aa840862901a37fa714b6f5
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-02-17 16:42:54 +0000

    move build() to bp_utils.py

--- backport/backport.py
+++ backport/backport.py
@@ -80,19 +80,6 @@ def patch_info(state):
     print_log("@@patch_info", state)
     prompted_show_sha_info(state)
 
-def build(state):
-    print_log("@@build", state)
-    line_fn = lambda line, state: print_log(line, state)
-    prompt_to_set_or_alter_state('build_cmd', state)
-    if not state['build_cmd']:
-        return
-
-    # do it, ignore ret text(for now @ least)
-    # assume build_cmd is of the form: <cmd> <arg string>
-    L = state['build_cmd'].split(' ')
-    shell_args = ' '.join(L[1:])
-    poll_shell_cmd(L[0], shell_args, line_fn, state)
-
 def backup_branch(state):
     print_log("@@backup_branch", state)
     line_fn = lambda line, state: print_log(line, state)
--- backport/bp_utils.py
+++ backport/bp_utils.py
@@ -361,3 +361,18 @@ def top_cites_upstream_or_confirmed_dont_care(state):
         return True
 
     return False
+
+def build(state):
+    print_log("@@build", state)
+    line_fn = lambda line, state: print_log(line, state)
+    prompt_to_set_or_alter_state('build_cmd', state)
+    if not state['build_cmd']:
+        return
+
+    # do it, ignore ret text(for now @ least)
+    # assume build_cmd is of the form: <cmd> <arg string>
+    L = state['build_cmd'].split(' ')
+    shell_args = ' '.join(L[1:])
+    spew = poll_shell_cmd(L[0], shell_args, line_fn, state)
+    return spew
+
