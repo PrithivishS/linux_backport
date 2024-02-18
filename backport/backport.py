b79bc7660af9cf79f9af2738131643c3a80b9d89
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-02-18 18:34:11 +0000

    add git log menu item to resolve menu

--- backport/backport.py
+++ backport/backport.py
@@ -253,6 +253,7 @@ bp_menu_item_list = [
         {'prompt' : 'build and parse', 'action': get_needed_symbols},
         {'prompt' : 'short display unresolved syms',
          'action': short_display_unres_symbols},
+        {'prompt' : 'find commits referring to pattern', 'action' : git_logG},
         {'prompt' : 'detailed display unresolved sym by index',
          'action': detailed_display_unres_symbol_by_index}
     ],
