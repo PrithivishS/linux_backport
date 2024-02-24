d04ceac15a6767038ed6479cd6120ed26d404faa
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-02-24 19:46:42 +0000

    move broken build() to old_build() revive older version, fix eventually

--- backport/bp_utils.py
+++ backport/bp_utils.py
@@ -359,6 +359,21 @@ def build(state):
     if not state['build_cmd']:
         return
 
+    # do it, ignore ret text(for now @ least)
+    # assume build_cmd is of the form: <cmd> <arg string>
+    L = state['build_cmd'].split(' ')
+    shell_args = ' '.join(L[1:])
+    spew = poll_shell_cmd(L[0], shell_args, line_fn, state)
+    print_log(spew, state)
+    return spew
+
+def old_build(state):
+    print_log("@@build", state)
+    line_fn = lambda line, state: print_log(line, state)
+    prompt_to_set_or_alter_state('build_cmd', state)
+    if not state['build_cmd']:
+        return
+
     (ret, spew) = shell_cmd(state['build_cmd'])
     return spew
 
