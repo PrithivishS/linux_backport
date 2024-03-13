7b97498a0aa8ca4dec1980a806383fc22f0624db
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-03-13 14:18:13 +0000

    show_stuff_for_conflict_resolution() - blame current and prev version of files

--- backport/bp_utils.py
+++ backport/bp_utils.py
@@ -764,10 +764,20 @@ def show_stuff_for_conflict_resolution(state):
                                             cherry_pick_sha,
                                             os.path.split(file)[1])
         shell_cmd(cmd)
+
+        # show git blame for previous rev of this file
         cmd = "git blame %s^ -- %s > %s/%s/prev.blame.%s" % (
             cherry_pick_sha, file, state['cherry_pick_files'],
             cherry_pick_sha, os.path.split(file)[1])
         shell_cmd(cmd)
+
+        #git blame for current rev
+        cmd = "git blame %s -- %s > %s/%s/blame.%s" % (
+            cherry_pick_sha, file, state['cherry_pick_files'],
+            cherry_pick_sha, os.path.split(file)[1])
+        shell_cmd(cmd)
+        
+
         
     # FIXME: maybe later show diff beteween local file and sha version
     
