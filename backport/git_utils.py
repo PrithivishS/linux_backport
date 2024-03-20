986bf9aa5afd00f4a8b27dc7a31fb050e93ec2b3
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-03-20 16:32:48 +0000

    add git_get_conflicts()

--- backport/git_utils.py
+++ backport/git_utils.py
@@ -280,3 +280,7 @@ def get_short_sha_list_by_tag(tag):
     out = out.split("\n")
     return out
 
+def git_get_conflicts():
+    cmd = "git diff --diff-filter=U"
+    (ret, out) = shell_cmd(cmd)
+    return out
