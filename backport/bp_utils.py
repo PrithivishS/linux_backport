f39d3304b9e05f773a4f0ae2e02fb8ef3348b920
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-03-20 17:28:03 +0000

    HACKERY - INTEGRATE THIS OR DROP IT

--- backport/bp_utils.py
+++ backport/bp_utils.py
@@ -1086,3 +1086,10 @@ def delete_one_log_result(state):
     del state['git_log_results'][key]
 
     
+def hackery(state):
+    for patch in state['all_patches']:
+        patch['conflicts'] = None
+        patch['prev_conflicts'] = None
+        patch['downstream'] = None
+        patch['prev_downstream'] = None
+        
