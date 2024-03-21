620e92d36a42a12f0db8d8af9eb23ef37d496822
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-03-21 17:45:31 +0000

    commit_order_in_tag_sha_list(): improve readability of function and output

--- backport/bp_utils.py
+++ backport/bp_utils.py
@@ -687,13 +687,13 @@ def commit_order_in_tag_sha_list(state, tag, sha):
     if not tag: return -1
     slbt = state['sha_lists_by_tag']
     if not slbt:
-        print("sha_lists_by_tag not in state")
+        print(f"sha_lists_by_tag not in state: {tag}")
         state['sha_lists_by_tag'] = dict()
         slbt = state['sha_lists_by_tag']
+
     if tag not in slbt:
         L = git_utils.get_short_sha_list_by_key(tag)
         slbt[tag] = L
-        state['sha_lists_by_tag']
     else:
         L = slbt[tag]
 
