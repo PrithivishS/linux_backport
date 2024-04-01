ff262ff4885ab08a4e13a160cfdbae2802123077
Eric Van Tassell <Eric.VanTassell@amd.com>
2024-04-01 14:40:57 +0000

    state module

--- backport/bp_utils.py
+++ backport/bp_utils.py
@@ -39,22 +39,6 @@ def prompt_to_set_or_alter_state(key, d): #: core, @io, @ui, @state, @persist
             d[key] = tmp
 #@@@
 
-def state_init(args): # anoter obvious objuect #: state, @persist
-    state = dict()
-    state['cherry_pick_files'] = "/tmp/cp__files"
-    state['all_patches'] = list()
-    state['pickle_dir'] = args.pickle_dir
-    state['pickle_file'] = args.pickle_file
-    state['first_commit'] = args.first_commit
-    state['args'] = args
-    state['sha_lists_by_tag'] = dict()
-    #TBD:, FIXME:  get rid of state fields from args that !change
-    state['patch_list_history'] = []
-    state['backport_in_progress'] = False
-    state['sha_to_patch'] = dict()
-    state['downstream_sha_to_patch'] = dict()
-    return state
-
 def backport_patches(state, menu_item_list): #: @core, @MAIN
     current_date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     pl.print_log("Startup: Current Time = %s" %(current_date_time), state)
