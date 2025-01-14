69403a22744f5761d1b864babc12bda0b22403b6
evantass@amd.com <evantass@amd.com>
2025-01-14 11:10:12 -0800

    remove debug code

--- rvw_diff.py
+++ rvw_diff.py
@@ -70,7 +70,6 @@ def diff_list(L1, L2):
         if count == 0 :
             print("no non context diff**")
         else:
-            pdb.set_trace()
             print(buf)
 
 def get_commit_parts(sha):
